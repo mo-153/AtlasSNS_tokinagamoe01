c0a9e13c3c4621d0457312171cf0ab20ff0ff708
moe19 <moe19@i.softbank.jp>
2025-03-03 00:28:23 +0900

    Removed script.js from public/js

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-// アコーディオンメニュ
-
-$(function () {
-  $('.accordion-menu').on('click', function () {
-    $(this).find('.accordion').toggleClass('open');
-    $(this).find('.allow').toggleClass('rotated');
-    $(this).find('.accordion-content').slideToggle();
-  });
-});
