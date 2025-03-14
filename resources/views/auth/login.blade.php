27ecfc7defb20985d4ad36c886f5d7b0fb0749a2
moe19 <moe19@i.softbank.jp>
2025-03-14 23:55:36 +0900

    ログインページの作成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,34 @@
-<x-logout-layout>
+<x-logout-layout >
 
   <!-- 適切なURLを入力してください -->
   {!! Form::open(['url'=>'login','method'=>'post']) !!}
 
-  <p>AtlasSNSへようこそ</p>
+<div class = "box">
+    <p class = "text">AtlasSNSへようこそ</p>
+<!-- <form action="URL">
+  <p>mail address</p>
+  <input type="text">
+  <p>password</p>
+    <input type="password">
+</form> -->
 
+<div class = "login-form">
   {{ Form::label('email') }}
   {{ Form::text('email',null,['class' => 'input']) }}
   {{ Form::label('password') }}
   {{ Form::password('password',['class' => 'input']) }}
+</div>
 
-  {{ Form::submit('ログイン') }}
+<!-- <div class = "login-btn">
+  {{ Form::submit('LOGIN') }}
+</div> -->
 
-  <p><a href="register">新規ユーザーの方はこちら</a></p>
 
-  {!! Form::close() !!}
+<button class = "login-btn">LOGIN</button>
+<p><a href="register" class = "new-users">新規ユーザーの方はこちら</a></p>
+</div>
+
+</div>
+{!! Form::close() !!}
 
 </x-logout-layout>
