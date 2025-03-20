5cb601259ffd0f0222269fc2d1f131a532715f02
moe19 <moe19@i.softbank.jp>
2025-03-20 09:43:41 +0900

    新規ユーザー登録画面のレイアウト実装

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,35 +1,36 @@
 <x-logout-layout>
     <!-- 適切なURLを入力してください -->
-{!! Form::open(['url'=>'logout', 'method'=>'post']) !!}
+{!! Form::open(['url'=>'register', 'method'=>'post']) !!}
     <!-- form action:新規登録のデータを送信するためのもの -->
-    !!Form:open(['url' ->'register'])!!
+    {!! Form::open(['url' =>'register']) !!}
         @csrf
-
         {!! Form::open(['route' => 'register','method' => 'POST']) !!}
 
-        <h2>新規ユーザー登録</h2>
 
-        {{ Form::label('ユーザー名') }}
-        {{ Form::text('username',null,['class' => 'input']) }}
+        <div class = "new-box">
+            <p class = "user-registration">新規ユーザー登録</p>
+
+            <div class = "login-form">
+            {{ Form::label('user name') }}
+            {{ Form::text('username',null,['class' => 'input']) }}
+
+            {{ Form::label('mail address') }}
+            {{ Form::email('email',null,['class' => 'input']) }}
+
+             {{ Form::label('password') }}
+             {{ Form::text('password',null,['class' => 'input']) }}
+
+             {{ Form::label('password confirm') }}
+             {{ Form::password('password_confirmation',null,['class' => 'input', 'placeholder' => '確認用パスワード']) }}
+
+             <button class = "new-user-btn">REGISTER</button>
+            </div>
+
+                <p><a href="login" class = "back-page">ログイン画面へ戻る</a></p>
+        </div>
+
+            {!! Form::close() !!}
+        </Form:open>
 
 
-{{ Form::label('メールアドレス') }}
-{{ Form::email('email',null,['class' => 'input']) }}
-
-{{ Form::label('パスワード') }}
-{{ Form::text('password',null,['class' => 'input']) }}
-
-
-{{ Form::label('パスワード確認') }}
-{{ Form::text('password_confirmation',null,['class' => 'input']) }}
-
-
-{{ Form::submit('登録') }}
-
-<p><a href="login">ログイン画面へ戻る</a></p>
-
-{!! Form::close() !!}
-</Form:open>
-
-
-</x-logout-layout>
+    </x-logout-layout>
