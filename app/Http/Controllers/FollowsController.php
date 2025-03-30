845e431a22fda81c4541e35201027b13351f66ea
moe19 <moe19@i.softbank.jp>
2025-03-30 22:42:03 +0900

    フォロー実装中

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -13,4 +13,29 @@ class FollowsController extends Controller
     public function followerList(){
         return view('follows.followerList');
     }
+
+
+
+    // フォローについて
+    public function follow(Request $request)
+    {
+        $followerId = Auth::id();
+        $userId = $request->input('user_id');
+
+        $follow = Follow::where('following_id',$followerId)
+        ->where('followed_Id',$userId)
+        ->first();
+
+        if($follow){
+            $follow->delete();
+            // フォロー解除
+        }else{
+            // フォロー
+            Follow::create([
+                'following_id'=>$followerId,
+                'followed_id'=>$userId,
+            ]);
+        }
+        return redirect('/search');
+    }
 }
