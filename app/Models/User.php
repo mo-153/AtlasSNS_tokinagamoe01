4f50876b06141fd49cedc091c8ea6d1d2d1ca75e
moe19 <moe19@i.softbank.jp>
2025-03-30 22:43:31 +0900

    フォロー実装中

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,76 +2,49 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Model;
-
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+
+    // リレーション
     protected $fillable = [
         'username',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-
-        // フォローする
-    public function follow(Int $User_id)
+    // フォローする
+    public function follow(Int $user_id)
     {
-        return $this -> follows()->attach($user_id);
+        return $this->follows()->attach($user_id);
     }
+
     // フォロー解除する
-    public function unfollow(Int $User_id)
+    public function unfollow(Int $user_id)
     {
-        return $this -> follows()->detach($user_id);
+        return $this->follows()->detach($user_id);
     }
 
     // フォローしているか
-    public function isFollowing(Int $User_id)
+    public function isFollowing(Int $user_id)
     {
-        return (boolean) $this-> follows()->where('followed_id',$user_id)->first(['id']);
+        return (boolean) $this->follows()->where('followed_id', $user_id)->exists();
     }
 
     // フォローされているか
-    public function isFollowed(Int $User_id)
+    public function isFollowed(Int $user_id)
     {
-        return (boolean)$this -> follows()->where('following_id',$user_id)->first(['id']);
+        return (boolean) $this->follows()->where('following_id', $user_id)->exists();
     }
 }
-
-
-    // リレーション設定
-     class Follow extends Model
-   {
-     use HasFactory;
-            public function following(){
-            return $this -> belongToMany (followed::class)->withTimestamps();
-            }
-        }
-
-        // belongToMany()
-        // →多対多のリレーションシップを設定できる
-        // withTimestamps()
-        // →タイムスタンプ（）が自動的に保存する
-        // →中間テーブルはタイムスタンプが自動されないから記載する
