7ebc68b9ba497facba5e57e1062098c1839d5e1c
moe19 <moe19@i.softbank.jp>
2025-03-30 22:46:11 +0900

    フォロー実装中

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,31 +1,39 @@
-<?php
+<<?php
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     //profileは自分で記述
     // ユーザーのプロフィールページにアクセスするためのメソッド
-    view/users/profile.blade.phpにビューを返す
+    // view/users/profile.blade.phpにビューを返す
     public function profile(){
         return view ('users.profile');
     }
 
     // ユーザーを検索するページにアクセスするためのメソッド
     // view/users/search.blade.phpにビューを返す
-    public function search(){
-        return view('users.search');
-    }
+    public function search(Request $request)
+{
+    // 入力された検索キーワードを取得
+    $keyword = $request->input('search');
+
+    // 検索結果を取得 (例として、ユーザー名にキーワードを部分一致で検索)
+    $users = User::where('username', 'LIKE', "%{$keyword}%")->get();
+
+    // ビューに検索結果を渡す
+    return view('users.search', ['date' => $users]);
+}
 
     // followも自分で記述
     // フォローページにアクセスするためのメソッド
     // views/follow.blade.phpにビューを返す
-    public function follow(){
+    public function followPage(){
         return view('follow');
     }
-
 
 
     // フォロー
@@ -36,10 +44,10 @@ class UsersController extends Controller
 
         $is_following = $follower->isFollowing($user->id);
         if(!$is_following){
-　　　　// フォローしてなかったらフォローする
-            $follower->follow($user->id);
-            return back();
-        }
+    // フォローしてなかったらフォローする
+    $follower->follow($user->id);
+    return back();
+      }
     }
 
     // フォロー解除
@@ -49,11 +57,10 @@ class UsersController extends Controller
         // フォローしているか
 
         $is_following = $follower->isFollowing($user->id);
-        if($is_following){
-        // フォローしてなかったらフォローする
-            $follower->unfollow($user->id);
-            return back();
-        }
+        if ($is_following) {
+    // フォロー解除する
+    $follower->unfollow($user->id);
+    return back();
+     }
     }
-
 }
