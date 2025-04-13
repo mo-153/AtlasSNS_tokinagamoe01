0e5f9aa3d7fe9f76774c5743e3bfc45ee5358dca
moe19 <moe19@i.softbank.jp>
2025-04-13 11:55:18 +0900

    フォローボタンの実装

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Follow;
 
 class FollowsController extends Controller
 {
@@ -28,14 +30,32 @@ class FollowsController extends Controller
 
         if($follow){
             $follow->delete();
+            return response()->json(['status' =>false]);
             // フォロー解除
+
         }else{
             // フォロー
             Follow::create([
                 'following_id'=>$followerId,
                 'followed_id'=>$userId,
             ]);
+            return response()->json(['status'=>true]);
         }
-        return redirect('/search');
+    }
+
+
+    public function unfollow(Request $request)
+    {
+        $followerId = Auth::id();
+        $userId = $request->input('user_id');
+
+        $follow = Follow::where('following_id',$followerId)
+        ->where('followed_id',$userId)
+        ->first();
+
+        if($follow){
+            $follow->delete();
+            return response()->json(['status'=>false]);
+        }
     }
 }
