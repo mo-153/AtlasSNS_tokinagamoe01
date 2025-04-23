7252fc89bdd7be8be56149ec1ddb7c7a1b278b65
moe19 <moe19@i.softbank.jp>
2025-04-24 05:59:09 +0900

    投稿表示実装中

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,24 +9,60 @@ class PostsController extends Controller
 {
 
     // すべてのユーザーの投稿を表示
-    // public function index(){
-    //     return view('posts.index');
-    // }
+    public function index(){
+        $posts=Post::all();
+        return view('posts.index',compact('posts'));
+    }
 
-    // public function show(){
-    //     $posts=Post::get();
-    //     return view('posts.index',compact('posts'));
-    // }
 
+    // バリデーション
+    // 投稿をするときのルール
+    // 【Post】
+    // ・入力必須
+    // ・1文字以上、150字以内
+
+        /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validatedDate = $request->validate(['post'=>'required|min:1|max:150 ']);
+
+        return view('posts.store');
+    }
+
+
+
+
+
+// すべての投稿と各投稿者の名前の表示
+    public function show(){
+        $posts=Post::get();
+        return view('posts.index',compact('posts'));
+    }
 
 
     // フォローユーザのみの投稿を表示
-    public function show(){
-        $following_id = Auth::user()->follows()->pluck('following_id');
-        // ↑フォローしているユーザーのidを取得
-        $posts = Post::with('user')->whereIn('user_id',$following_id)->get();
-        // ↑フォローしているユーザーのidをもとに投稿内容を取得
+    // public function show(){
+    //     ↑show:表示という意味
+    //     $following_id = Auth::user()->follows()->pluck('following_id');
+    //     ↑フォローしているユーザーのidを取得
+    //     $posts = Post::with('user')->whereIn('user_id',$following_id)->get();
+    //     ↑フォローしているユーザーのidをもとに投稿内容を取得
 
-        return view ('posts.index',compact('posts'));
-    }
+    //     return view ('posts.index',compact('posts'));
+    // }
 }
+
+// ↑フォローしているユーザーのみの情報を取得する
+// public function show(){
+// フォローしているユーザーのidを取得
+//   $following_id = Auth::user()->follows()->pluck(' ① ');
+
+// フォローしているユーザーのidを元に投稿内容を取得
+//   $posts = Post::with('user')->whereIn(' ② ', $following_id)->get();
+
+//   return view('yyyy', compact(‘posts’));
+// }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,8 +1,28 @@
 <x-login-layout>
-      <form action="{{ route('posts.index') }}" method = "get">
-        @foreach($posts as $post)
-        <p>名前:{{ $post->user->username }}</p>
-        <p>投稿内容:{{ $post->post }}</p>
-        @endforeach
-      </form>
+<!-- 投稿フォーム -->
+<div class="new-post-icon">
+  <img src="{{ asset('images/icon' . (Auth::id() % 7 + 1) . '.png') }}">
+</div>
+  <form action="{{ route('posts.index') }}" method = "get">
+    <textarea name="new-post-form" class="new-post-form" placeholder="投稿内容を入力してください。"></textarea>
+    <button id = "post-form-btn"><i class = "fas fa-post"></i>
+      <img src="{{ asset('images/post.png') }}" class="post-btn">
+    </button>
+</form>
+
+
+<!-- 自分＆フォローしている人の投稿を表示 -->
+<div class="post-form">
+  <div class="post-form-icon">
+      <img src="{{ asset('images/icon' . (Auth::id() % 7 + 1) . '.png') }}">
+        <form action="{{ route('posts.index') }}" method = "get">
+    <textarea name="post-form-before" class="post-form-before"></textarea></textarea>
+
+  </div>
+  @foreach($posts as $post)
+  <p>名前:{{ $post->user->username }}</p>
+  <p>投稿内容:{{ $post->post }}</p>
+  @endforeach
+</div>
+
 </x-login-layout>
