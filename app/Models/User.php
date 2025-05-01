f1fb7eb3619e831fe8946dcfe1c2a3c46822c708
moe19 <moe19@i.softbank.jp>
2025-05-02 05:41:29 +0900

    リレーション

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,10 @@ class User extends Authenticatable
         'username',
         'email',
         'password',
+        'following_id',
+        'followed_id',
     ];
+    // protected $fillableを記述することでusername, email,password,following_id,followed_idの登録ができるようになる
 
     protected $hidden = [
         'password',
@@ -41,13 +44,13 @@ class User extends Authenticatable
     // フォローしているか
     public function isFollowing(Int $user_id)
     {
-        return (boolean) $this->follows()->where('followed_id', $user_id)->exists();
+        return $this->follows()->where('followed_id', $user_id)->exists();
     }
 
     // フォローされているか
-    public function isFollowed(Int $user_id)
+    public function isFollowedBy(Int $user_id)
     {
-        return (boolean) $this->follows()->where('following_id', $user_id)->exists();
+        return $this->follows()->where('user_id', $user_id)->exists();
     }
 
     public function posts()
@@ -59,6 +62,81 @@ class User extends Authenticatable
     // belongsToManyを記述してUserモデルトFollowテーブルの紐づけ
     // フォローしているユーザーの情報を取得する
     public function follows(){
-        return $this->belongsToMany(User::class,'follow_user','following_id','followed_id');
+        return $this->belongsToMany(User::class,'follows','following_id','followed_id');
     }
+
+    // フォローされているユーザーの情報を取得する
+    public function followers(){
+        return $this->belongsToMany(User::class,'follows','followed_id','following_id');
+    }
+
+
+
+
+
+
+
+
+
+    // ↓元々Follow.phpに記述していた内容
+        public function user(){
+        // １対多の関係
+        return $this ->belongsTo('App/User');
+    }
+
+    // リレーション設定
+    // このユーザーがフォローしている人
+    public function following()
+    {
+        // 多対多の関係
+        return $this->belongsToMany(
+            User::class, // 関連するモデル
+            'follows',   // 中間テーブル
+            'following_id',
+            'followed_id'
+        )->withTimestamps();
+    }
+
+    // このユーザーをフォローしている人
+    public function followed()
+    {
+        // 多対多の関係
+        return $this->belongsToMany(
+            User::class, // 関連するモデル
+            'follows',   // 中間テーブル
+            'followed_id',
+            'following_id'
+            )->withTimestamps();
+        }
+
+        // belongToMany()
+        // →多対多のリレーションシップを設定できる
+        // withTimestamps()
+        // →タイムスタンプ（）が自動的に保存する
+        // →中間テーブルはタイムスタンプが自動されないから記載する
+
+
+
+        // 多対多：リレーションについて
+        //  return $this->belongsToMany{
+        // '1','2','3','4'};
+
+        // 1には関係するモデルの場所を記載
+        // 例）move.phpなど多対多の菅家のモデルの名前を記入
+        // ディレクトリの場所を間違えないように注意
+
+        // ２中間テーブルの名前を記載
+        // 例）actor_moves
+
+        // 3中間テーブルにある自分のIDが入るカラムを記入
+        // 例）actor_id
+
+        // ４中間テーブルの相手モデルに関係してるカラムを記入
+        // 例）move_id
+
+
+
+
+
+
 }
