07d334744c48192b2ab54139e278efde56fea3d1
moe19 <moe19@i.softbank.jp>
2025-05-02 05:37:40 +0900

    投稿フォーム実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,28 +1,30 @@
 <x-login-layout>
-<!-- 投稿フォーム -->
+
+<!-- 新規投稿フォーム -->
 <div class="new-post-icon">
   <img src="{{ asset('images/icon' . (Auth::id() % 7 + 1) . '.png') }}">
 </div>
-  <form action="{{ route('posts.index') }}" method = "get">
+
+  <form action="{{ route('posts.store') }}" method = "post">
+    @csrf
     <textarea name="new-post-form" class="new-post-form" placeholder="投稿内容を入力してください。"></textarea>
     <button id = "post-form-btn"><i class = "fas fa-post"></i>
       <img src="{{ asset('images/post.png') }}" class="post-btn">
+
     </button>
 </form>
 
 
 <!-- 自分＆フォローしている人の投稿を表示 -->
-<div class="post-form">
-  <div class="post-form-icon">
-      <img src="{{ asset('images/icon' . (Auth::id() % 7 + 1) . '.png') }}">
-        <form action="{{ route('posts.index') }}" method = "get">
-    <textarea name="post-form-before" class="post-form-before"></textarea></textarea>
-
+<div class="post-icon-before">
+  <img src="{{ asset('images/icon' . (Auth::id() % 7 + 1) . '.png') }}">
   </div>
-  @foreach($posts as $post)
-  <p>名前:{{ $post->user->username }}</p>
-  <p>投稿内容:{{ $post->post }}</p>
-  @endforeach
-</div>
 
-</x-login-layout>
+  <article class="post-form-before">
+    @foreach($posts as $post)
+    <p>名前:{{ $post->user->username }}</p>
+    <p>投稿内容:{{ $post->post }}</p>
+    @endforeach
+  </article>
+
+  </x-login-layout>
