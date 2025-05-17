8ed218b48418f44152e24caf216348e5a73a7a39
moe19 <moe19@i.softbank.jp>
2025-05-18 07:23:36 +0900

    フォローするボタンの切り替え

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -11,18 +11,6 @@ class FollowsController extends Controller
 {
 
 
-
-// フォロー数カウント
-public function FollowCount(){
-    $follows = Follow::where('user_id',$followerId)->get();
-    return
-}
-// フォローワー数カウント
-public function FollowerCount(){
-
-}
-
-
     // フォローについて
     public function follow(Request $request)
     {
@@ -45,23 +33,6 @@ public function FollowerCount(){
             ]);
             return response()->json(['status'=>true]);
             // フォローする
-        }
-    }
-
-
-    // フォロー解除について
-    public function unfollow(Request $request)
-    {
-        $followerId = Auth::id();
-        $userId = $request->input('user_id');
-
-        $follow = Follow::where('following_id',$followerId)
-        ->where('followed_id',$userId)
-        ->first();
-
-        if($follow){
-            $follow->delete();
-            return response()->json(['status'=>false]);
         }
     }
 }
--- public/js/script.js
+++ public/js/script.js
@@ -11,35 +11,33 @@ $(function () {
 
 
 // フォローボタンの切り替え
-$(document).ready(function () {
-  // ↑ページの読み込みが完了したら処理が開始する
-  $(".follow-toggle").click(function (event) {
-    event.preventDefault();
-    // ↑event.preventDefault();でクリックしてもページが移動しないようにする
+$(document).on("click", ".follow-toggle", function (event) {
+  event.preventDefault();
 
-    var button = $(this);
-    var userId = button.data("user-id");
-    var isFollowed = button.attr("data-follow") === "true";
-    $.ajax({
-      url: isFollowed ? "/unfollow" : "/follow",
-      // ↑三項演算子（条件式の真偽に応じて２つの値のいずれかを片方に返すこと）
-      type: "POST",
-      data: {
-        user_id: userId,
-        _token: $('meta[name="csrf-token"]').attr("content")
-      },// ←セキュリティ対策
-      success: function (response) {
-        if (response.status) {
-          button.text("フォロー解除").removeClass("btn-primary").addClass("btn-danger");
-          // ↑ボタンの表示を「フォロー解除」に変更
-          button.data("follow", "true");
-        } else {
-          button.text("フォローする").removeClass("btn-danger").addClass("btn-primary");
-          // ↑ボタンの表示を「フォローする」に変更
+  var button = $(this);
+  var userId = button.data("user-id");
+  // ↑event.preventDefault();でクリックしてもページが移動しないようにする
 
-          button.data("follow", "false");
-        }
+  $.ajax({
+    url: "/follow",
+    // ↑三項演算子（条件式の真偽に応じて２つの値のいずれかを片方に返すこと）
+    type: "POST",
+    data: {
+      user_id: userId,
+      _token: $('meta[name="csrf-token"]').attr("content")
+    },// ←セキュリティ対策
+
+    success: function (response) {
+      if (response.status) {
+        button.text("フォロー解除").removeClass("btn-primary").addClass("btn-danger");
+        // ↑ボタンの表示を「フォロー解除」に変更
+        button.data("follow", "true");
+      } else {
+        button.text("フォローする").removeClass("btn-danger").addClass("btn-primary");
+        // ↑ボタンの表示を「フォローする」に変更
+
+        button.data("follow", "false");
       }
-    });
+    },
   });
 });
