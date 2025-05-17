b416305b26bb228cb55ea014be4e632e55af23be
moe19 <moe19@i.softbank.jp>
2025-05-18 07:20:20 +0900

    フォロー数カウント

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -86,9 +86,9 @@
     </div>
   <footer>
   </footer>
-  <script src="{{ asset('js/app.js') }}"></script>
 </div>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 <script src="{{ asset('js/script.js') }}"></script>
 </body>
 </html>
