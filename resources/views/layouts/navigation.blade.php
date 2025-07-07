ddfae36dab551a028c6286494bffd045ed646135
moe19 <moe19@i.softbank.jp>
2025-07-07 21:49:20 +0900

    asset関数追加

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
-        <div id="head">
-        <h1><a href = "top"><img src="images/atlas.png" alt="Atlas"></a></h1>
+<div id="head">
+        <h1><a href = "top"><img src="{{ asset('images/atlas.png') }}" alt="Atlas"></a></h1>
       <!-- ↑<a href="">「""」内に飛びたいURLを記載-->
-       <!-- <body> -->
+      <!-- ↑{{asset()}}関数で画像を表示 -->
          <div id="header">
               <div id="../posts/index.blade.php">
         </div>
