eb5fccb68a67c4e5656dbd6680ee9e7c431300ee
moe19 <moe19@i.softbank.jp>
2025-08-11 20:09:44 +0900

    ヘッダーのロゴ調整

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,7 @@
         <div id="head">
-        <h1><a href = "top"><img src="{{ asset('images/atlas.png') }}" alt="Atlas"></a></h1>
+        <h1><a href = "/top"><img src="{{ asset('images/atlas.png') }}" alt="Atlas" class="logo"></a></h1>
       <!-- ↑<a href="">「""」内に飛びたいURLを記載-->
+        <!-- ↑絶対パスでTOPページに戻れる(/top) -->
          <div id="header">
               <div id="../posts/index.blade.php">
         </div>
