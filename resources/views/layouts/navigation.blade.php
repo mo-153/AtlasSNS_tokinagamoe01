6acd40cb5d39046b347c4425c300f6d0f5494202
moe19 <moe19@i.softbank.jp>
2025-08-12 22:31:37 +0900

    TOPページに飛ぶように実装

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
         <div id="head">
-        <h1><a href = "/top"><img src="{{ asset('images/atlas.png') }}" alt="Atlas" class="logo"></a></h1>
+        <h1><a href = "{{route('posts.index')}}"><img src="{{ asset('images/atlas.png') }}" alt="Atlas" class="logo"></a></h1>
       <!-- ↑<a href="">「""」内に飛びたいURLを記載-->
         <!-- ↑絶対パスでTOPページに戻れる(/top) -->
          <div id="header">
